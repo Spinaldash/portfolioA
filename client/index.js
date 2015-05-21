736ac2c4f35d78bc7b28b98d45cf8051df024052
Spinaldash <blueavenger@gmail.com>
2015-05-21 14:04:14 -0700

    adding section navbar

--- client/index.js
+++ client/index.js
@@ -1,4 +1,11 @@
 'use strict';
 $(document).ready(function() {
-    $('#fullpage').fullpage();
+    $('#fullpage').fullpage({
+      verticleCentered: false,
+      fitToSection: false,
+			navigation: true,
+      anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage'],
+			navigationPosition: 'right',
+			navigationTooltips: ['Intro', 'Portfolio', 'Summary', 'Contact']
+    });
 });
